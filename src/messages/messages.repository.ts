3eef3e5fa4706d542de91249c187f299f75ab4af
LongNT <longnt@ql6625.fr>
2021-08-01 18:46:28 +0700

    Update repository storage file path

--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -1,27 +1,31 @@
+import * as path from 'path';
 import { readFile, writeFile } from "fs/promises";
 
 export class MessagesRepository {
+    // filePath = path.join(__dirname, 'messages.json');
+    filePath = 'messages.json';
+
     async findOne(id: string) {
-        const contents = await readFile('messages.json', 'utf-8');
+        const contents = await readFile(this.filePath, 'utf-8');
         const messages = JSON.parse(contents);
 
         return messages[id];
     }
 
     async findAll() {
-        const contents = await readFile('messages.json', 'utf-8');
+        const contents = await readFile(this.filePath, 'utf-8');
         const messages = JSON.parse(contents);
 
         return messages;
     }
 
     async create(content: string) {
-        const contents = await readFile('messages.json', 'utf-8');
+        const contents = await readFile(this.filePath, 'utf-8');
         const messages = JSON.parse(contents);
 
         const id = Math.floor(Math.random() * 999);
         messages[id] = {id, content};
 
-        await writeFile('message.json', JSON.stringify(messages));
+        await writeFile(this.filePath, JSON.stringify(messages));
     }
 }
