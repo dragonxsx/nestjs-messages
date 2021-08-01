266359598724b91a3e4750595b771fad28413f6c
LongNT <longnt@ql6625.fr>
2021-08-01 18:45:29 +0700

    Update controller logic

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,20 +1,29 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { createMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+    messagesService: MessagesService;
+
+    constructor() {
+        // DON'T DO THIS ON REAL APP
+        // USE DEPENDENCY INJECTION
+        this.messagesService = new MessagesService();
+    }
+
     @Get()
     listMessages() {
-
+        return this.messagesService.findAll();
     }
 
     @Post()
     createMessage(@Body() body: createMessageDto) {
-        console.log(body);
+        return this.messagesService.create(body.content);
     }
 
     @Get('/:id')
     getMessage(@Param('id') id: string) {
-        
+        return this.messagesService.findOne(id);
     }
 }
